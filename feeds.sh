8519eca0a6a7bb7a945899eca60341421d89b98c
smalls <smalls0098@gmail.com>
2022-03-30 11:47:22 +0800

    change file

---
+++ feeds.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Copyright (c) 2021-2022 smalls0098 <https://smalls0098.com>
+#
+# This is free software, licensed under the BSD 3-Clause License.
+# See /LICENSE for more information.
+#
+# https://github.com/cnlowkey/OpenWrt-XiaomiR4A
+# Description: Build OpenWrt using GitHub Actions
+
+cp -rf /feeds/packages/lang/golang/golang-package.mk /feeds/lang/golang/golang-package.mk
