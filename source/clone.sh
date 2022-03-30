0f9b50758e0563c65502ac3a352d7202d1eb390d
smalls <smalls0098@gmail.com>
2022-03-30 21:19:15 +0800

    change file

--- source/clone.sh
+++ source/clone.sh
@@ -21,18 +21,18 @@ git clone https://github.com/coolsnowwolf/lede.git
 
 # 下载自定义插件
 rm -rf lede/package/lean/luci-theme-argon
-#sed -i '$a src-git openwrt_packages https://github.com/openwrt/packages' feeds.conf.default
-#sed -i '$a src-git luci_openwrt https://github.com/openwrt/luci.git' feeds.conf.default
-#sed -i '$a src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small_packages https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '$a src-git kenzok8_packages https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git openwrt_packages https://github.com/openwrt/packages' feeds.conf.default
+sed -i '$a src-git luci_openwrt https://github.com/openwrt/luci.git' feeds.conf.default
+sed -i '$a src-git kiddin9_packages https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small_packages https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzok8_packages https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 cd lede/package
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-#git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon.git
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon.git
 git clone -b master --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 git clone -b master --single-branch https://github.com/kenzok8/openwrt-packages.git
 git clone -b main --single-branch https://github.com/Ausaci/luci-app-nat6-helper.git
